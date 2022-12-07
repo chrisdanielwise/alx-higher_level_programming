b959d3bf17116fcb0479c305e93db88352efb0d3
chrisdanielwise <chrisdanielwise@gmail.com>
2022-12-07 20:08:59 +0000

    Hot fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
-def print_sorted_dictionary(a_dictionary):
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
         print(f"{key:s}: {a_dictionary.get(key)}")
